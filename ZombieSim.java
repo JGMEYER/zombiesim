27fdd8406fec96292e22ebe0be28a89a7fc20762
Justian Meyer <meyer.justian@gmail.com>
2015-01-28 13:19:18 -0500

    Changed actor count to be more easily modifiable.

--- ZombieSim.java
+++ ZombieSim.java
@@ -13,6 +13,7 @@ public class ZombieSim extends Application {
 
     public static final int CANVAS_WIDTH = 800;
     public static final int CANVAS_HEIGHT = 600;
+    public static final int NUM_STARTING_ACTORS = 3000;
 
     private Pane canvas;
     private ArrayList<Actor> actors;
@@ -31,7 +32,7 @@ public class ZombieSim extends Application {
 
         actors = new ArrayList<Actor>();
         // create actors
-        for (int i = 0; i < 3000; i++) {
+        for (int i = 0; i < NUM_STARTING_ACTORS; i++) {
             double x = Math.random() * CANVAS_WIDTH;
             double y = Math.random() * CANVAS_HEIGHT;
 
